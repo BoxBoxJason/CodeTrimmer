68665ca41d8f93bc8b48b55bf81bd2fa1e8f1c01
BoxBoxJason <62643679+BoxBoxJason@users.noreply.github.com>
2023-09-14 23:03:44 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,6 +2,13 @@
 '''
 Created on 21 juil. 2023
 
+User friendly tool to cleanup your project folders/files.
+Removes trailing spaces at the end of every line of your project for increased readability.
+You only need to specify the root folder/file to cleanup, the program will explore everything recursively
+
+Works with python 3.6 and up.
+Requires only one argument: the path (absolute or relative) to your source folder/file that you need to trim.
+
 @author: BoxBoxJason
 '''
 import logging
