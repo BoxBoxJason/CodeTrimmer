a0287d0e0626d1d20a67087f2a0ffcedf0d3fdb0
BoxBoxJason <62643679+BoxBoxJason@users.noreply.github.com>
2023-09-14 22:41:29 +0200

    Code files

---
+++ __main__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 21 juil. 2023
+
+@author: BoxBoxJason
+'''
+import logging
+import os
+import sys
+
+from Trimmer import trimChildren
+from Trimmer import trimFile
+
+##---------- Logging setup ----------##
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(os.path.join(os.path.dirname(__file__),"logging.log")),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+##---------- Argument Parser ----------##
+if len(sys.argv) != 2:
+    logging.fatal(TypeError,f"Incorrect number of arguments ({len(sys.argv[1]) -1}/1)")
+    sys.exit(1)
+
+rootFolderPath = sys.argv[1]
+
+if os.path.exists(rootFolderPath) and os.access(rootFolderPath,os.R_OK):
+    if os.path.isdir(rootFolderPath):
+        trimChildren(rootFolderPath)
+    else:
+        trimFile(rootFolderPath)
+    logging.info(f"End of trimming, check if everything went smooth in {rootFolderPath}")
+else:
+    logging.fatal(FileNotFoundError,f"File or Directory not found or access denied at {rootFolderPath}")
